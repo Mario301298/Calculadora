d5468b8f226c6e8dd8efb3788909ba72134fd149
Mario301298 <mario301298@gmail.com>
2020-04-06 19:40:53 +0200

    Subiendo el fichero a GitHub

---
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculadora;
+
+// @author Mario López Baños
+
+public class Calculadora {
+    
+    private int num1;
+    private int num2;
+    
+    public Calculadora (int a, int b) {
+        
+        num1 = a;
+        num2 = b;
+        
+    }
+    
+    public int suma() {
+        
+        int result = num1+num2;
+        return result;
+        
+    }
+    
+    public int resta() {
+        
+        int result;
+        
+        if (resta2()) result = num1 - num2;
+        else          result = num2 - num1;
+        
+        return result;
+        
+    }
+    
+    public boolean resta2() {
+        
+        if (num1 >= num2) return true;
+        else              return false;
+        
+    }
+    
+    public Integer divide2() {
+        
+        if (num2==0) return null;
+        int result = num1 / num2;
+        return result;
+        
+    }
+    
+    
+    public int multiplica() {
+        
+        int result = num1*num2;
+        return result;
+        
+    }
+    
+    public int divide() {
+        
+        int result = num1/num2;
+        return result;
+        
+    }
+    
+    public int divide0(){
+        if(num2==0)
+            throw new java.lang.ArithmeticException("División por 0");
+            else{
+            int resul=num1/num2;
+            return resul;
+        }
+    }
+    
+}
